ed7591e4845de42395e5a7c58308bd770451c693
Monica <monica.babaian@gmail.com>
2018-03-04 15:04:17 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log('JS file connected!')
+
+
+// isotope
+var $gallery = $('#gallery').isotope({
+    // options
+    itemSelector: '.col-sm-4 .photo',
+    layoutMode: 'masonry',
+    masonry: {
+        gutter:5
+    }
+  });
+
+  // always use imagesLoaded when using isotope
+  $gallery.imagesLoaded().progress(function() {
+      $gallery.isotope('layout');
+  })
+
+  // filter content based on id
+  var $container = $("#gallery");
+
+  $('.btn').on('click', function() {
+
+    var filterValue = $(this).attr('data-filter');
+    $container.isotope({ filter: filterValue });
+    $(this).addClass('checked');
+  });
